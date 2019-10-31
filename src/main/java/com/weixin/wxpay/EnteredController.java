f02abcd22455a485b4c4911bd9bb213859334c1d
smileyf-wb <13439485354@139.com>
2019-10-31 10:12:35 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	pom.xml

--- src/main/java/com/weixin/wxpay/EnteredController.java
+++ src/main/java/com/weixin/wxpay/EnteredController.java
@@ -4,8 +4,21 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class EnteredController {
 
+    /**
+     * 活动报名
+     * @return
+     */
     @RequestMapping("/signup")
     public String hello(){
         return "entered";
+    }
+
+    /**
+     * 活动详情
+     * @return
+     */
+    @RequestMapping("/activityDetail")
+    public String activity(){
+        return "activityDetail";
     }
 }
