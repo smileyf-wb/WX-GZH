1be03ec9ed397d4431d88b2f0dadc33caee7ebac
13439485354@139.com <13439485354@139.com>
2019-11-09 19:25:44 +0800

    Merge branch 'master' of https://github.com/smileyf-wb/WX-GZH
    # Conflicts:
    #	.idea/workspace.xml

--- src/main/java/com/weixin/entity/EnteredDetail.java
+++ src/main/java/com/weixin/entity/EnteredDetail.java
@@ -1,11 +1,60 @@
 package com.weixin.entity;
 
+import java.io.Serializable;
+
 /**
  * 报名详情信息
  */
-public class EnteredDetail {
+public class EnteredDetail implements Serializable {
 
+    /**
+     * 用户id
+     */
+    private String openId;
+    /**
+     * 姓名
+     */
     private String name;
 
-    
+    /**
+     * 性别
+     */
+    private String sex;
+
+    /**
+     * 班级
+     */
+    private String classes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    public String getOpenId() {
+        return openId;
+    }
+
+    public void setOpenId(String openId) {
+        this.openId = openId;
+    }
 }
--- src/main/java/com/weixin/wxpay/EnteredController.java
+++ src/main/java/com/weixin/wxpay/EnteredController.java
@@ -1,18 +1,31 @@
 package com.weixin.wxpay;
+import com.alibaba.fastjson.JSON;
 import com.weixin.common.Order;
+import com.weixin.entity.EnteredDetail;
 import com.weixin.wxpay.security.CheckRepeatEntered;
+import com.weixin.wxpay.service.RedisService;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class EnteredController {
 
+    @Autowired
+    RedisService redisService;
     /**
-     * 活动报名
+     * 报名详情
      * @return
      */
-    @CheckRepeatEntered
+
     @RequestMapping("/signup")
     public String hello( Model model,Order order){
         /*String trade_fee = order.getTrade_fee();
@@ -32,5 +45,57 @@ public class EnteredController {
         return "activityDetail";
     }
 
+    /**
+     * 报名信息存放在redis中，并设置过期时间
+     * @return
+     */
+    @CheckRepeatEntered
+    @RequestMapping("/baoming")
+    public String pay(HttpServletRequest request, HttpServletResponse response, EnteredDetail enteredDetail){
 
+        String name = enteredDetail.getName();
+       String enteredDetailJson = JSON.toJSONString(enteredDetail);
+       String openId = enteredDetail.getOpenId();
+         long expireTime = 2*60*60;
+        redisService.set(openId,enteredDetailJson,expireTime);
+         return "pay";
+    }
+
+    @RequestMapping(value="pay_finish")
+    public String payFinish(@RequestParam("reult")String payResult,@RequestParam("openId")String openId, HttpServletRequest request,HttpServletResponse response,Model model){
+       if("get_brand_wcpay_request:ok".equals(payResult)){
+            //支付成功
+            String userEnteredInfo=(String) redisService.get(openId);
+           EnteredDetail enteredDetail =  JSON.parseObject(userEnteredInfo,EnteredDetail.class);
+           //存入数据库
+
+           model.addAttribute("payResult","支付成功");
+        }else{
+           model.addAttribute("payResult","支付成功");
+        }
+
+        return "payResult";
+    }
+
+    @RequestMapping(value="/main")
+    public ModelAndView main(Model model){
+        List<EnteredDetail> enteredDetailLsit = new ArrayList<>();
+        EnteredDetail enteredDetail = new EnteredDetail();
+        enteredDetail.setName("张三");
+        enteredDetail.setSex("女");
+        enteredDetail.setClasses("1020561");
+
+        EnteredDetail enteredDetail1 = new EnteredDetail();
+        enteredDetail1.setName("张三");
+        enteredDetail1.setSex("女");
+        enteredDetail1.setClasses("1020561");
+
+        enteredDetailLsit.add(enteredDetail);
+        enteredDetailLsit.add(enteredDetail1);
+
+        model.addAttribute("enteredDetailLsit",enteredDetailLsit);
+        model.addAttribute("title","报名信息");
+        return new ModelAndView("main","userModel",model);
+
+    }
 }
